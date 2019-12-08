06244c80746029ed114ec36f6769c7bee712a328
Esteban Debole <estebandebole@gmail.com>
2019-12-08 20:39:28 -0300

    probando heroku v3

--- db/migrate/20191127025526_create_reservations.rb
+++ db/migrate/20191127025526_create_reservations.rb
@@ -3,7 +3,6 @@ class CreateReservations < ActiveRecord::Migration[6.0]
     create_table :reservations do |t|
       t.references :client, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
-      t.references :sell, null: true, foreign_key: true
       t.datetime :reservation_date
 
       t.timestamps
